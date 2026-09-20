dfa34a617ecafa8cabf2a159946d2ca026a89e48
aiclaudehiggsfield3-dot <aiclaudehiggsfield3@gmail.com>
2026-09-20 19:05:01 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// Google Apps Script 웹 앱을 배포한 뒤, 그 주소(https://script.google.com/macros/s/.../exec)를 여기에 넣으세요.
-// 비워 두면 홈페이지는 data/tracker.js 의 정적 데이터만 보여줍니다(회의록 작성 기능은 숨김).
-window.CONFIG = { API: "" };
+window.CONFIG = { API: "https://script.google.com/macros/s/AKfycbyLNtTOi019HSITwoU0Ei59iBntl2CRjmJa5S8JhVx9_R3uW-Wo3tCA5DkHYlQrnoOI/exec" };
